0c07f1e31f914f1a7ddd92d356c32883533ee2bf
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-05 13:24:21 +0200

    Added changes for Movies, NavBar, TVShows and Auth components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router";
+
 function NavBar() {
-    return ;
+  return (
+    <div className="p-[2rem] border bg-neutral-700 text-white rounded">
+      <nav className="grid grid-cols-3">
+        <div>
+          <Link to="">
+            <span>Log In</span>
+          </Link>
+        </div>
+        <div className="flex justify-between">
+          <Link to="">
+            <span>Homepage</span>
+          </Link>
+          <Link to="">
+            <span>Movies</span>
+          </Link>
+          <Link to="">
+            <span>TVShows</span>
+          </Link>
+          <Link to="">
+            <span>Bookmarks</span>
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default NavBar;
