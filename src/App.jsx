71a9f9275f0adf4e2692c62283d3346ff9e7bd7f
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-05 14:58:38 +0200

    Edited NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar.jsx";
+import { useState } from "react";
 
 export default function App() {
+
+const [authType, setAuthType] = useState("login");
+const [loggedIn, setLoggedIn] = useState("")
+
+
   return (
     <>
-      <NavBar />
-      <Outlet />
+      <NavBar setAuthType={setAuthType} loggedIn={loggedIn}/>
+      <Outlet context={{authType, setAuthType, loggedIn, setLoggedIn}} />
     </>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router";
 
-function NavBar() {
+function NavBar(props) {
+  const {setAuthType, loggedin } = props;
+
+  const sigin = () => {
+    setAuthType("sigin");
+  };
+
+  const login = () => {
+    setAuthType("login");
+  };
   return (
     <div className="p-[2rem] border bg-neutral-700 text-white rounded">
       <nav className="grid grid-cols-3">
-        <div>
-        </div>
+        <div></div>
         <div className="flex justify-between">
           <Link to="">
             <span>Homepage</span>
@@ -21,9 +29,31 @@ function NavBar() {
           </Link>
         </div>
         <div className="text-right">
-          <Link to="/authorization">
-            <span>Log In</span>
-          </Link>
+          {!loggedin ? (
+            <div>
+              <Link to="/authorization">
+                <button
+                  className="me-2"
+                  type="button"
+                  value="login"
+                  onClick={login}
+                >
+                  Log In
+                </button>
+              </Link>
+              <Link to="/authorization">
+                <button
+                  className="ms-2"
+                  type="button"
+                  onClick={sigin}
+                >
+                  Sign up
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <div>ShowIcon</div>
+          )}
         </div>
       </nav>
     </div>
