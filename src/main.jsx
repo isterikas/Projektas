b28c2140e2a006c09fbb2eb506611b28b99f5bae
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-05 12:55:46 +0200

    Updated  Main.jsx whit Routes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,24 @@ import "./index.css";
 import App from "./App.jsx";
 import { Routes, Route } from "react-router";
 import NotFound from "./components/NotFound.jsx";
+import Auth from "./components/Auth.jsx";
+import Homepage from "./components/Homepage.jsx";
+import Movies from "./components/Movies.jsx";
+import TVShows from "./components/TVShows.jsx";
+import Bookmarks from "./components/Bookmarks.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-       <Route path ="/" element={<App/>}/>
-       <Route path ="*" element={<NotFound/>}/>
+        <Route path="/" element={<App />}>
+          <Route index element={Homepage} />
+          <Route path="/authorization" element={Auth} />
+          <Route path="/movies" element={Movies} />
+          <Route path="/tvshows" element={TVShows} />
+          <Route path="/bookmarks" element={Bookmarks} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
