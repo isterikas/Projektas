c96362eb1aa6ce79ad9f95a549aa80b363e3e1e4
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-05 19:27:36 +0200

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,58 @@
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { getAllData } from "./components/helpers/get.js";
+
 
 export default function App() {
+  const [authType, setAuthType] = useState("login");
+  const [loggedIn, setLoggedIn] = useState("");
 
-const [authType, setAuthType] = useState("login");
-const [loggedIn, setLoggedIn] = useState("")
+  const [contents, setContents] = useState("");
+  const [users, setUsers] = useState("")
+  const [userBookmarks, setUserBookmarks] = useState("")
+  const [error, setError] = useState("");
 
+  const getAllcontents = async () => {
+    try {
+      const contents = await getAllData("contents");
+      setContents(contents);
+      setError("")
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const getAllUsers = async () => {
+    try {
+      const users = await getAllData("users");
+      setUsers(users);
+      setError("")
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const getAllUserBookmarks = async () => {
+    try {
+      const userBookmarks = await getAllData("userBookmarks");
+      setUserBookmarks(userBookmarks);
+      setError("")
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() =>{
+    getAllcontents()
+    getAllUsers()
+    getAllUserBookmarks()
+  }, [])
 
   return (
     <>
-      <NavBar setAuthType={setAuthType} loggedIn={loggedIn}/>
-      <Outlet context={{authType, setAuthType, loggedIn, setLoggedIn}} />
+      <NavBar setAuthType={setAuthType} loggedIn={loggedIn} />
+      { <p>{error}</p> || <Outlet context={{ authType, setAuthType, loggedIn, setLoggedIn, contents, setContents, users, setUsers, userBookmarks, setUserBookmarks }} /> }
     </>
   );
 }
