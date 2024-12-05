bb0d9ffdc5cc881fd0bcffc948a9a55dda87de1d
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-05 12:34:24 +0200

    Edited App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
 import { Routes, Route } from "react-router";
+import NotFound from "./components/NotFound.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
        <Route path ="/" element={<App/>}/>
+       <Route path ="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   </StrictMode>
