f0ba1aef2b603d9d8cca2456890e061240dd3090
Eryk Tatol <eryk.tatol@stud.techin.lt>
2024-12-06 13:06:02 +0200

    search functionality was implemented for movies page

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,45 +1,35 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { url } from "./helpers/jsonURL.js";
+import { useState } from "react";
+import Card from "./Card.jsx";
+import { useLocation } from "react-router";
 
-function Search() {
+function Search({ array }) {
   const [search, setSearch] = useState("");
-  const [error, setError] = useState("");
-  const [content, setContent] = useState([]);
-  console.log(search);
-  console.log(error);
+  const location = useLocation();
 
-  const getAllData = async () => {
-    const response = await axios.get(url("contents"));
-    return response.data;
-  }; //tailored getAllData function; will have to be deleted once fetch will be executed on the main page.
-
-  const fetchData = async () => {
-    try {
-      const data = await getAllData();
-      setContent(data);
-    } catch (error) {
-      setError(error.message);
+  const getPlaceholder = () => {
+    switch (location.pathname) {
+      case "/movies":
+        return "Search for movies";
+      case "/tvshows":
+        return "Search for TV Shows";
+      case "/bookmarks":
+        return "Search for bookmarked shows";
+      default:
+        return "Search for movies or TV series";
     }
-  }; //will have to be deleted and array of content should be passed through props, params or context.
+  };
 
-  useEffect(() => {
-    fetchData();
-  }, [search]);
-
-  const filteredArray = content
-  .filter((content) =>
-    content.title.toLowerCase().includes(search.toLowerCase())
-  )
-  .map((item) => {
-    return (
-      <>
-        <p>{item.title}</p>
-
-
-      </>
-    );
-  })
+  const filteredArray = array
+    .filter((content) =>
+      content.title.toLowerCase().includes(search.toLowerCase())
+    )
+    .map((item) => {
+      return (
+        <>
+          <Card item={item} key={item.contentsId} />
+        </>
+      );
+    });
 
   return (
     <>
@@ -48,13 +38,20 @@ function Search() {
           onChange={(e) => setSearch(e.target.value)}
           className="nosubmit rounded bg-red-500"
           type="search"
-          placeholder="Search for movies or TV series"
+          placeholder={getPlaceholder()}
         />
       </form>
 
       <div className="bg-teal-600 p-3 grid grid-cols-3">
         {filteredArray} <br />
-        {search? <h1 className="font-bold">Found {filteredArray.length} {filteredArray.length === 1? "result" : "results"} for "{search}"</h1> : ""}
+        {search ? (
+          <h1 className="font-bold">
+            Found {filteredArray.length}{" "}
+            {filteredArray.length === 1 ? "result" : "results"} for "{search}"
+          </h1>
+        ) : (
+          ""
+        )}
       </div>
     </>
   );
