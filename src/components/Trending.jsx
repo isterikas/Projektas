2a91286c0011fefe028df1ca75f43c333172b771
D2JP <d2jp@TECHIN.INTERNAL>
2024-12-06 11:54:44 +0200

    Modefy Treading

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
