a0f06195a798742ba5894dfaefd0d5ea09c0e0b8
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-07 23:58:27 +0200

    Created comp-icons folder whit icon componenets inside & modified NavBar.jsx

---
+++ src/components/comp-icons/icon-hamburger.jsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { NavLink } from "react-router";
+
+function HamburgerMenu(props) {
+  const { setAuthType, setLoggedIn } = props;
+
+  const [isOpen, setIsOpen] = useState("");
+
+  const toggleMenu = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const sigin = () => {
+    setAuthType("signup");
+    setLoggedIn("");
+  };
+
+  const login = () => {
+    setAuthType("login");
+    setLoggedIn("");
+  };
+
+  return (
+    <div className="relative ">
+      {/* Hamburger Icon */}
+      <button
+        onClick={toggleMenu}
+        className="flex flex-col w-[25px] h-[20px] md:w-[32px] md:h-[26px] justify-around"
+      >
+        <span className="block w-full h-[4px] bg-white "></span>
+        <span className="block w-full h-[4px] bg-white "></span>
+        <span className="block w-full h-[4px] bg-white"></span>
+      </button>
+
+      {/* Dropdown Menu */}
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        }   bg-[#161D2F] absolute  left-[-120%] md:left-[-80%] rounded-md w-[5rem] text-center mt-2 z-[999]`}
+      >
+        <NavLink to="/authorization">
+          <p
+            type="button"
+            onClick={login}
+            className="text-white block p-2 hover:bg-gray-200 hover:text-[#161D2F] rounded-md font-semibold"
+          >
+            Log In
+          </p>
+        </NavLink>
+        <NavLink to="/authorization">
+          <p
+            type="button"
+            onClick={sigin}
+            className="text-white block p-2 hover:bg-gray-200 hover:text-[#161D2F] rounded-md font-semibold"
+          >
+            Sign up
+          </p>
+        </NavLink>
+      </div>
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black opacity-50"
+          onClick={() => setIsOpen(false)}
+        ></div>
+      )}
+    </div>
+  );
+}
+
+export default HamburgerMenu;
