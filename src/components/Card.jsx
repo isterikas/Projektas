bad26717c84d7c837c27d6a294ad19cf97167045
Audrius <a.adomavicius.pastas@gmail.com>
2024-12-09 11:05:13 +0200

    css tailwind

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,28 @@
 
 function Card({ item }) {
 
-    const {thumbnail, title, year, category, rating, contentsId } = item;
-   
+  const { thumbnail, title, year, category, rating, contentsId } = item;
 
-    return (
-      <div className="shadow m-3">
-        <div><img src={"src" + thumbnail.regular.small.slice(1)} alt="#" /></div>
-        <p className="body-s text-white"> {year}</p>
-        <p className="body-s text-white"> {category}</p>
-        <p className="body-s text-white"> {rating}</p>
-        <h1 className="heading-xs text-white"> {title}</h1>
+
+  return (
+
+    <div className="columns-1">
+      <div className="movies mx-3  ">
+        <div className=" " >< img className="  rounded-lg " src={"src" + thumbnail.regular.small.slice(1)} alt="#" />  
+        <span className="material-symbols-outlined absolute ">
+          bookmark
+        </span>
+         </div>
+        
+        <div>
+          <p className="body-s text-white"> {year}</p>
+          <p className="body-s text-white"> {category}</p>
+          <p className="body-s text-white"> {rating}</p>
+          <h1 className="heading-xs text-white"> {title}</h1>
+        </div>
       </div>
-      );
+    </div>
+  );
 }
 
 export default Card;
