7f4ed874740b220bb4f1d3db28a5a3994667bcec
Edgaras Laptevas <edgaraslaptevas88gmail.com>
2024-12-09 09:01:02 +0200

    Formated all components

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,9 +3,14 @@ import { Link } from "react-router";
 function NotFound() {
   return (
     <div className="text-center mt-[10rem]">
-      <h1 className="text-[5rem] p-4">404  Page is Not Found!</h1>
+      <h1 className="text-[5rem] p-4">404 Page is Not Found!</h1>
       <Link to="/">
-        <button type="button" className="bg-slate-500 text-xl px-5 py-3 rounded-md text-white">Go Back!</button>
+        <button
+          type="button"
+          className="bg-slate-500 text-xl px-5 py-3 rounded-md text-white"
+        >
+          Go Back!
+        </button>
       </Link>
     </div>
   );
--- src/components/navbar-components/icons-navbar.jsx
+++ src/components/navbar-components/icons-navbar.jsx
@@ -31,7 +31,6 @@ const NavBarIcons = (props) => {
                 ? "fill-white transition duration-500"
                 : "fill-[#5A698F] hover:fill-red-500 transition duration-500"
             }`}
-            
           >
             <path d="M8 0H1C.4 0 0 .4 0 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11H1c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1ZM19 0h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1V1c0-.6-.4-1-1-1Zm0 11h-7c-.6 0-1 .4-1 1v7c0 .6.4 1 1 1h7c.6 0 1-.4 1-1v-7c0-.6-.4-1-1-1Z" />
           </svg>
