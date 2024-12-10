8bf827514ade7a57124eed1f5524ec0d0e182cdf
D2JP <d2jp@TECHIN.INTERNAL>
2024-12-10 14:08:02 +0200

    Css,fix bugs Trending

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { deleteBookmark } from "./helpers/delete.js";
 import { postData } from "./helpers/post.js";
 import movieIcon from "../assets/icons/icon-nav-movies.svg";
+import PlayIcon from "../assets/icons/icon-play.svg";
 
 function Card({ item, userBookmarks, setUpdate, update, loggedIn, width }) {
   const { thumbnail, title, year, category, rating, contentsId } = item;
@@ -36,26 +37,53 @@ function Card({ item, userBookmarks, setUpdate, update, loggedIn, width }) {
   };
 
   //window magic
-  
+
   //end of window magic
 
   return (
     <div className="shadow m-3 ">
       <div className="relative ">
         {loggedIn ? (
-          <button onClick={toggleBookmark} className="text-white absolute  bookmark-icon">
-            <img src={checked ? "src/assets/icons/icon-bookmark-full.svg" : "src/assets/icons/icon-bookmark-empty.svg"} alt="" />
+          <button
+            onClick={toggleBookmark}
+            className="text-white absolute  bookmark-icon"
+          >
+            <img
+              src={
+                checked
+                  ? "src/assets/icons/icon-bookmark-full.svg"
+                  : "src/assets/icons/icon-bookmark-empty.svg"
+              }
+              alt=""
+            />
           </button>
         ) : (
           ""
         )}
-        {{width} < 1024 ? (<img className="rounded-xl bottom-5" src={"src" + thumbnail.regular.small.slice(1)} alt="#" />) : (<img className="rounded-xl bottom-5" src={"src" + thumbnail.regular.large.slice(1)} alt="#" />)}
+        {{ width } < 1024 ? (
+          <img
+            className="rounded-xl bottom-5"
+            src={"src" + thumbnail.regular.small.slice(1)}
+            alt="#"
+            
+          />
+        ) : (
+          <img
+            className="rounded-xl bottom-5"
+            src={"src" + thumbnail.regular.large.slice(1)}
+            alt="#"
+          />
+        )}
       </div>
 
       <div className="flex mt-3 gap-2 relative">
         <p className="body-s text-white"> {year}</p>
         <p className="body-s  text-white ">.</p>
-        <img className="body-s mt-0.5 w-[15px] h-[15px]" src={movieIcon} alt="SVG Image" />
+        <img
+          className="body-s mt-0.5 w-[15px] h-[15px]"
+          src={movieIcon}
+          alt="SVG Image"
+        />
         <p className="body-s text-white"> {category}</p>
         <p className="body-s text-white">.</p>
         <p className="body-s text-white"> {rating}</p>
